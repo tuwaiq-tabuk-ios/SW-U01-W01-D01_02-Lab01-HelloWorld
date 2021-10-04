11b912c816ce7653384170ea12d7d2005ee9221d
خالد <khald@MacBook-Air-khald.local>
2021-10-04 17:00:27 +0300

    Created and completed the HelloWorld project

---
+++ Ahlam/Ahlam/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Ahlam
+//
+//  Created by خالد on 27/02/1443 AH.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Ahlam/Ahlam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ahlam
+//
+//  Created by خالد on 27/02/1443 AH.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
