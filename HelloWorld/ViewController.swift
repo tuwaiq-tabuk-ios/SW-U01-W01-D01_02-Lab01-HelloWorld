e7eaf082dabfc055265b5f6c86d46280c8d24b68
MarzouqAlmukhlif <Marzouq.Almukhlif.tuu.s@tuwaiq.edu.sa>
2021-10-04 13:27:29 +0300

    Push my project HelloWorld

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Marzoog Almoklif on 27/02/1443 AH.
+//  Copyright © 1443 Marzoog Almoklif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
