21091b934d67fc931c02e87c480a0f68dd2b846b
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-10-04 15:34:26 +0300

    Created and completed the HelloWorld project

---
+++ helloward/helloward/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloward
+//
+//  Created by عبدالعزيز البلوي on 27/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
