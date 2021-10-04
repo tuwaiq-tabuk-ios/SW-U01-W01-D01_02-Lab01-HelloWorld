4d0a4cf615607bf2f43eb8214d0d8309fddfa935
RAWABI BAJAFAR <91871676+RawabiBajafar@users.noreply.github.com>
2021-10-04 16:10:52 +0300

    Created and completed the HelloWorld project

---
+++ Rawabi apple/Rawabi appleTests/Rawabi_appleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rawabi_appleTests.swift
+//  Rawabi appleTests
+//
+//  Created by روابي باجعفر on 27/02/1443 AH.
+//
+
+import XCTest
+@testable import Rawabi_apple
+
+class Rawabi_appleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
