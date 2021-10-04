2b94d0a4b3f74f852a8246c9e367a9901f6bdb8c
Ahmed awadh alqhtani <a7mdalq@MacBook-Air-Ahmed.local>
2021-10-04 15:20:11 +0300

    HelloWorld finished

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ahmed awadh alqhtani on 27/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
