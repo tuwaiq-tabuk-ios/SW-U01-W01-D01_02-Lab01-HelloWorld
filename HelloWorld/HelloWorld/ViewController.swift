a6996ac5e8813ee23b255c094b873e291eba25e6
HAJAR <hajar@HAJARs-MacBook-Pro.local>
2021-10-04 20:29:50 +0300

    Created and completed the HelloWorld project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by HAJAR on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
