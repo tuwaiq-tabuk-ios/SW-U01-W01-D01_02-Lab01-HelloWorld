fa623c2ff80ca1a7753e5bd7c73e3596e61d2ef5
Reema Mousa <reemamousa@MacBook-Pro-Reema.local>
2021-10-04 14:25:01 +0300

    created and completed the HelloWorld project

---
+++ HelloWord2/HelloWord2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWord2
+//
+//  Created by Reema Mousa on 27/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
