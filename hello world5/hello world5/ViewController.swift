6cf2ef65b4ab38e2e3e18fe93d0fa920365af60c
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-10-04 16:50:33 +0300

    Hello World App finished

---
+++ hello world5/hello world5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello world5
+//
+//  Created by Shorouq AlAnzi on 27/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
